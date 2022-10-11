ded01c0e220b23b103494b5fb6552c2d5f9534d2
Ste-cracco <s.cracco@yahoo.it>
2022-10-11 15:41:45 +0200

    Errore connessione datatbase

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Movie;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    //
+    public function index() 
+    {
+        $movies = Movie::all();
+        
+
+        return view('home');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app')
+
+@section('headText', 'Homepage Movies')
+    
+@section('content')
+    <h1>Movies</h1>
+@endsection
